f9e729f69e09da19dd87e6813ee278b03782a1df
tk <tk@square-hiptobe.com>
2012-07-26 19:42:00 +0000

    Initial Import.

---
+++ MyTime/MyTime/MainPage.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Microsoft.Phone.Controls;
+
+namespace MyTime
+{
+	public partial class MainPage : PhoneApplicationPage
+	{
+		private DateTime _timerBase;
+		private TimeSpan _timer;
+		private DispatcherTimer _dt;
+
+		[System.ComponentModel.EditorBrowsable]
+		public Object Icon { get; set; }
+		private enum TimerState
+		{
+			Stopped,
+			Paused,
+			Running
+		};
+
+		private TimerState _timerState = TimerState.Stopped;
+
+
+		// Constructor
+		public MainPage()
+		{
+			InitializeComponent();
+
+			// Set the data context of the listbox control to the sample data
+			DataContext = App.ViewModel;
+			this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+
+
+		}
+
+		// Load data for the ViewModel lbRvItems
+		private void MainPage_Loaded(object sender, RoutedEventArgs e)
+		{
+			if (!App.ViewModel.IsDataLoaded) {
+				App.ViewModel.LoadData();
+				App.ViewModel.LoadMainMenu();
+			}
+		}
+
+		private void abibStart_Click(object sender, EventArgs e) 
+		{ 
+			switch(_timerState) {
+				case TimerState.Running:
+					break;
+				case TimerState.Paused:
+					_timerBase = DateTime.Now.Subtract(_timer);
+					_dt.Start();
+					break;
+				default:		// TimerState.Stopped
+					_timerBase = DateTime.Now;
+					_dt = new DispatcherTimer() {
+					                            	Interval = new TimeSpan(0, 0, 0, 1, 0)
+					                            };
+					_dt.Tick += new EventHandler(dt_Tick);
+					_dt.Start();
+					_timer = new TimeSpan(0, 0, 1);
+					break;
+			}
+			_timerState = TimerState.Running;
+			lblTimer.Text = string.Format("{0:0,0}:{1:0,0}:{2:0,0}", _timer.Hours, _timer.Minutes, _timer.Seconds);
+		}
+
+		private void abibPause_Click(object sender, EventArgs e)
+		{
+			switch(_timerState) {
+				case TimerState.Paused:
+				case TimerState.Stopped:
+					abibStart_Click(sender, e);
+					break;
+				default: // TimerState.Running
+					_dt.Stop();
+					_timerState = TimerState.Paused;
+					break;
+			}
+		}
+
+		private void abibStop_Click(object sender, EventArgs e) 
+		{ 
+			_dt.Stop();
+			_timer = new TimeSpan();
+			_timerBase = new DateTime();
+			lblTimer.Text = "00:00:00";
+			_timerState = TimerState.Stopped;
+		}
+
+		private void dt_Tick(object sender, EventArgs e)
+		{
+			if (_timerState != TimerState.Running) {
+				_dt.Stop();
+				return;
+			}
+			_timer = DateTime.Now.Subtract(_timerBase);
+
+			lblTimer.Text = string.Format("{0:0,0}:{1:0,0}:{2:0,0}", _timer.Hours, _timer.Minutes, _timer.Seconds);
+		}
+
+		private void abmiManuallyEnter_Click(object sender, EventArgs e) { NavigationService.Navigate(new Uri("/ManuallyEnterTime.xaml", UriKind.Relative)); }
+
+		public void MenuItem_ClickEvent(object sender, RoutedEventArgs e) { }
+
+		public void MenuImage_TapEvent(object sender, System.Windows.Input.GestureEventArgs e) { }
+
+		private void imgAddReturnVisit_Tap(object sender, System.Windows.Input.GestureEventArgs e) { NavigationService.Navigate(new Uri("/AddNewRV.xaml", UriKind.Relative)); }
+
+		private void TextBox_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+		{
+			if(tbSearchRvs.Text.Equals("Search...", StringComparison.InvariantCultureIgnoreCase))
+			{
+				tbSearchRvs.Text = string.Empty;
+			}
+		}
+
+		private void tbSearchRvs_LostFocus(object sender, RoutedEventArgs e)
+		{
+			if(string.IsNullOrEmpty(tbSearchRvs.Text))
+			{
+				tbSearchRvs.Text = "Search...";
+			}
+		}
+	}
+}
---
+++ MyTime/MyTime/ViewModels/MainViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using MyTime.ViewModels;
+
+
+namespace MyTime
+{
+	public class MainViewModel : INotifyPropertyChanged
+	{
+		public MainViewModel()
+		{
+			this.lbRvItems = new ObservableCollection<ItemViewModel>();
+			lbMainMenuItems = new ObservableCollection<MainMenuViewModel>();
+		}
+
+		/// <summary>
+		/// A collection for ItemViewModel objects.
+		/// </summary>
+		public ObservableCollection<ItemViewModel> lbRvItems { get; private set; }
+		public ObservableCollection<MainMenuViewModel> lbMainMenuItems { get; private set; }
+
+		public bool IsDataLoaded
+		{
+			get;
+			private set;
+		}
+
+
+
+		/// <summary>
+		/// Creates and adds a few ItemViewModel objects into the lbRvItems collection.
+		/// </summary>
+		public void LoadData()
+		{
+			// Sample data; replace with real data
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime one", LineOne = "Maecenas praesent accumsan bibendum", LineTwo = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime two", LineOne = "Dictumst eleifend facilisi faucibus", LineTwo = "Suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus" });
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime three", LineOne = "Habitant inceptos interdum lobortis", LineTwo = "Habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent" });
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime four", LineOne = "Nascetur pharetra placerat pulvinar", LineTwo = "Ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos" });
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime five", LineOne = "Maecenas praesent accumsan bibendum", LineTwo = "Maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur" });
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime six", LineOne = "Dictumst eleifend facilisi faucibus", LineTwo = "Pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent" });
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime seven", LineOne = "Habitant inceptos interdum lobortis", LineTwo = "Accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat" });
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime eight", LineOne = "Nascetur pharetra placerat pulvinar", LineTwo = "Pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum" });
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime nine", LineOne = "Maecenas praesent accumsan bibendum", LineTwo = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime ten", LineOne = "Dictumst eleifend facilisi faucibus", LineTwo = "Suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus" });
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime eleven", LineOne = "Habitant inceptos interdum lobortis", LineTwo = "Habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent" });
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime twelve", LineOne = "Nascetur pharetra placerat pulvinar", LineTwo = "Ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos" });
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime thirteen", LineOne = "Maecenas praesent accumsan bibendum", LineTwo = "Maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur" });
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime fourteen", LineOne = "Dictumst eleifend facilisi faucibus", LineTwo = "Pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent" });
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime fifteen", LineOne = "Habitant inceptos interdum lobortis", LineTwo = "Accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat" });
+			lbRvItems.Add(new ItemViewModel() { Name = "runtime sixteen", LineOne = "Nascetur pharetra placerat pulvinar", LineTwo = "Pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum" });
+
+			this.IsDataLoaded = true;
+		}
+
+		public void LoadMainMenu()
+		{
+			lbMainMenuItems.Add(new MainMenuViewModel() { MenuText = "Add Time", MenuItemName = "miAddTime", IconUri = "/icons/add.png", MenuImageName = "AddTimeImage" });
+			lbMainMenuItems.Add(new MainMenuViewModel() { MenuText = "Add Return Visit", MenuItemName = "miAddRv", IconUri = "/icons/phone.png", MenuImageName = "AddReturnVisitImage" });
+			lbMainMenuItems.Add(new MainMenuViewModel() { MenuText = "Find Nearest RV", MenuItemName = "miFindNearRv", IconUri = "/icons/search.png", MenuImageName = "FindNearestRv" });
+			lbMainMenuItems.Add(new MainMenuViewModel() { MenuText = "Send Service Report", MenuItemName = "miSendReport", IconUri = "/icons/mail.png", MenuImageName = "SemdReportImage" });
+			lbMainMenuItems.Add(new MainMenuViewModel() { MenuText = string.Format("{0} Report",DateTime.Today.ToString("MMMM")), MenuItemName = "miThisMonthReport", IconUri = "/icons/favs.png", MenuImageName = "ThisMonthImage" });
+			lbMainMenuItems.Add(new MainMenuViewModel() { MenuText = string.Format("{0} Report",DateTime.Today.Year), MenuItemName = "miThisYearReport", IconUri = "/icons/favs.png", MenuImageName = "ThisYearImage" });
+			lbMainMenuItems.Add(new MainMenuViewModel() { MenuText = "Custom Report", MenuItemName = "miCustomReport", IconUri = "/icons/search.png", MenuImageName = "CustomReportImage" });
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void NotifyPropertyChanged(String propertyName)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (null != handler) {
+				handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
