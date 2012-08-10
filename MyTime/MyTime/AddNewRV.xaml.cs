f719219f29807d2b42446ecc51e54c78955ec3fb
tk <tk@square-hiptobe.com>
2012-08-10 22:14:21 +0000

    Added alot GUI functionality to AddNewRV.xaml page
    Started Building out the Database Model.

--- MyTime/MyTime/AddNewRV.xaml.cs
+++ MyTime/MyTime/AddNewRV.xaml.cs
@@ -1,32 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+using System.Device.Location;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Microsoft.Phone.Controls;
+using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Controls.Maps;
-using System.Device.Location;
+using Microsoft.Phone.Controls.Maps.Platform;
+using Microsoft.Phone.Scheduler;
+using Microsoft.Phone.Tasks;
 using MyTime.BingMapsGeocodeService;
+using GestureEventArgs = System.Windows.Input.GestureEventArgs;
 
 namespace MyTime
 {
 	public partial class AddNewRV : PhoneApplicationPage
 	{
-		public class States
-		{
-			public string Text {get; set;}
-		}
+		private readonly string _address;
+		private readonly string _cityStateZip;
+		private readonly string _fullName;
+		private GeocodeResult _currentBingGeocodeLocation;
+		private GeoCoordinateWatcher gcw;
 
-		System.Device.Location.GeoCoordinateWatcher gcw;
-
-		string _address, _fullName, _cityStateZip;
 		public AddNewRV()
 		{
 			InitializeComponent();
@@ -37,181 +32,206 @@ namespace MyTime
 			SetInfoText();
 		}
 
-		private void textBlock1_LostFocus(object sender, RoutedEventArgs e)
-		{
-
-		}
-
-		private void mapInfo_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
-		{
-			Pushpin p = new Pushpin();
-			p.Location = e.Position.Location;
-			mapInfo.Children.Clear();
-			mapInfo.Children.Add(p);
-			mapInfo.SetView(e.Position.Location, 12);
-		}
-
-		private void MakeReverseGeocodeRequest()
-		{
-			string Results = "";
-			try {
-				// Set a Bing Maps key before making a request
-				string key = "AkRhgqPR6aujo-xib-KiR8Lt20wsn89GY4R9SP0RA6h4w7QT9mS3kKwYKKxjklfV";
-
-				var reverseGeocodeRequest = new BingMapsGeocodeService.ReverseGeocodeRequest();
-
-				// Set the credentials using a valid Bing Maps key
-				reverseGeocodeRequest.Credentials = new GeocodeService.Credentials();
-				reverseGeocodeRequest.Credentials.ApplicationId = key;
-
-				// Set the point to use to find a matching address
-				BingMapsGeocodeService.GeocodeLocation.Location point = new GeocodeService.Location();
-				point.Latitude = 47.608;
-				point.Longitude = -122.337;
-
-				reverseGeocodeRequest.Location = point;
-
-				// Make the reverse geocode request
-				GeocodeService.GeocodeServiceClient geocodeService =
-				new GeocodeService.GeocodeServiceClient("BasicHttpBinding_IGeocodeService");
-				GeocodeService.GeocodeResponse geocodeResponse = geocodeService.ReverseGeocode(reverseGeocodeRequest);
-
-				Results = geocodeResponse.Results[0].DisplayName;
-
-			} catch (Exception ex) {
-				Results = "An exception occurred: " + ex.Message;
-
-			}
-
-		}
-
+		#region Events
 
 		private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
 		{
-			gcw = new System.Device.Location.GeoCoordinateWatcher();
+			gcw = new GeoCoordinateWatcher();
 			mapInfo.SetView(new GeoCoordinate(40, -95), 4);
-			gcw.PositionChanged += mapInfo_PositionChanged;
+			gcw.PositionChanged += currentLocation_PositionChanged;
 			gcw.Start(true);
-			List<States> states = new List<States>();
-			states.Add(new States() { Text = "Alabama" });
-			states.Add(new States() { Text = "Alaska" });
-			states.Add(new States() { Text = "American Samoa" });
-			states.Add(new States() { Text = "Arizona" });
-			states.Add(new States() { Text = "Arkansas" });
-			states.Add(new States() { Text = "California" });
-			states.Add(new States() { Text = "Colorado" });
-			states.Add(new States() { Text = "Connecticut" });
-			states.Add(new States() { Text = "Delaware" });
-			states.Add(new States() { Text = "District of Columbia" });
-			states.Add(new States() { Text = "Florida" });
-			states.Add(new States() { Text = "Georgia" });
-			states.Add(new States() { Text = "Guam" });
-			states.Add(new States() { Text = "Hawaii" });
-			states.Add(new States() { Text = "Idaho" });
-			states.Add(new States() { Text = "Illinois" });
-			states.Add(new States() { Text = "Indiana" });
-			states.Add(new States() { Text = "Iowa" });
-			states.Add(new States() { Text = "Kansas" });
-			states.Add(new States() { Text = "Kentucky" });
-			states.Add(new States() { Text = "Louisiana" });
-			states.Add(new States() { Text = "Maine" });
-			states.Add(new States() { Text = "Maryland" });
-			states.Add(new States() { Text = "Massachusetts" });
-			states.Add(new States() { Text = "Michigan" });
-			states.Add(new States() { Text = "Minnesota" });
-			states.Add(new States() { Text = "Mississippi" });
-			states.Add(new States() { Text = "Missouri" });
-			states.Add(new States() { Text = "Montana" });
-			states.Add(new States() { Text = "Nebraska" });
-			states.Add(new States() { Text = "Nevada" });
-			states.Add(new States() { Text = "New Hampshire" });
-			states.Add(new States() { Text = "New Jersey" });
-			states.Add(new States() { Text = "New Mexico" });
-			states.Add(new States() { Text = "New York" });
-			states.Add(new States() { Text = "North Carolina" });
-			states.Add(new States() { Text = "North Dakota" });
-			states.Add(new States() { Text = "Northern Marianas Islands" });
-			states.Add(new States() { Text = "Ohio" });
-			states.Add(new States() { Text = "Oklahoma" });
-			states.Add(new States() { Text = "Oregon" });
-			states.Add(new States() { Text = "Pennsylvania" });
-			states.Add(new States() { Text = "Puerto Rico" });
-			states.Add(new States() { Text = "Rhode Island" });
-			states.Add(new States() { Text = "South Carolina" });
-			states.Add(new States() { Text = "South Dakota" });
-			states.Add(new States() { Text = "Tennessee" });
-			states.Add(new States() { Text = "Texas" });
-			states.Add(new States() { Text = "Utah" });
-			states.Add(new States() { Text = "Vermont" });
-			states.Add(new States() { Text = "Virginia" });
-			states.Add(new States() { Text = "Virgin Islands" });
-			states.Add(new States() { Text = "Washington" });
-			states.Add(new States() { Text = "West Virginia" });
-			states.Add(new States() { Text = "Wisconsin" });
-			states.Add(new States() { Text = "Wyoming" });
-			lbiState.ItemsSource = states;
+		}
+
+		private void Pivot_LoadedPivotItem(object sender, PivotItemEventArgs e) { Focus(); }
+		private void Pivot_SelectionChanged(object sender, SelectionChangedEventArgs e) { SetInfoText(); }
+		private void appbar_addVisit_Click(object sender, EventArgs e) { }
+		private void appbar_cancel_Click(object sender, EventArgs e) { NavigationService.GoBack(); }
+		private void appbar_save_Click(object sender, EventArgs e) { }
+		private void btnCancel_Click(object sender, RoutedEventArgs e) { }
+		
+		private void btnDone_Click(object sender, RoutedEventArgs e)
+		{
+			try {
+				string reminderName = Guid.NewGuid().ToString();
+				// use guid for reminder name, since reminder names must be unique
+				Reminder reminder = new Reminder(reminderName);
+				// NOTE: setting the Title property is supported for reminders 
+				// in contrast to alarms where setting the Title property is not supported
+				reminder.Title = this.txtTitle.Text;
+				reminder.Content = this.txtContent.Text;
+
+				DateTime remindNow = new DateTime(dpDatePicker.Value.Value.Year, dpDatePicker.Value.Value.Month, dpDatePicker.Value.Value.Day, tpStartTime.Value.Value.Hour, tpStartTime.Value.Value.Minute, tpStartTime.Value.Value.Second);
+
+				var span = remindNow - DateTime.Now;
+
+				reminder.BeginTime = DateTime.Now.AddSeconds(span.TotalSeconds);
+
+				reminder.ExpirationTime = reminder.BeginTime.AddSeconds(5.0);
+				reminder.RecurrenceType = RecurrenceInterval.None;
+
+				ScheduledActionService.Add(reminder);
+
+				MessageBox.Show(string.Format("Reminder: '{0}' added.", txtTitle.Text), "Field Service App", MessageBoxButton.OK);
+
+				txtTitle.Text = string.Empty;
+				txtContent.Text = string.Empty;
+				dpDatePicker.Value = DateTime.Now;
+				tpStartTime.Value = DateTime.Now;
+			} catch {
+				MessageBox.Show("Could Not Add Reminder.");
+			} finally {
+				RefreshReminderList();
+			}
+		}
+
+		private void currentLocation_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+		{
+			var p = new Pushpin {Location = e.Position.Location};
+			mapCurrentAddress.Children.Clear();
+			mapCurrentAddress.Children.Add(p);
+			mapCurrentAddress.SetView(e.Position.Location, 12);
+			MakeReverseGeocodeRequest(e.Position.Location);
+		}
+
+		private void geocodeService_GeocodeCompleted(object sender, GeocodeCompletedEventArgs e)
+		{
+			var p = new Pushpin {Location = e.Result.Results[0].Locations[0]};
+			mapInfo.Children.Clear();
+			mapInfo.Children.Add(p);
+			mapInfo.SetView(p.Location, 11);
+		}
+
+		private void geocodeService_ReverseGeocodeCompleted(object sender, ReverseGeocodeCompletedEventArgs e)
+		{
+			GeocodeResult Results = e.Result.Results[0];
+			foreach (GeocodeResult r in e.Result.Results) {
+				if (r.Confidence == Confidence.High) Results = r;
+			}
+
+			lblCurrentAddress.Text = string.Format("{0}", Results.Address.AddressLine);
+			lblCurrentCityStateZip.Text = string.Format("{0}, {1} {2}", Results.Address.Locality, Results.Address.AdminDistrict, Results.Address.PostalCode);
+			_currentBingGeocodeLocation = Results;
+		}
+
+		private void mapCurrentAddress_Tap(object sender, GestureEventArgs e)
+		{
+			tbAddress1.Text = _currentBingGeocodeLocation.Address.AddressLine;
+			tbCity.Text = _currentBingGeocodeLocation.Address.Locality;
+			tbZipCode.Text = _currentBingGeocodeLocation.Address.PostalCode;
+			tbDistrict.Text = _currentBingGeocodeLocation.Address.AdminDistrict;
+		}
+
+		private void mapInfo_Tap(object sender, GestureEventArgs e)
+		{
+			var bmt = new BingMapsTask();
+			bmt.Center = mapInfo.Center;
+			bmt.ZoomLevel = 18;
+			bmt.Show();
+		}
+
+		private void miAddPicture_Click(object sender, EventArgs e) { }
+		private void miGetDirection_Click(object sender, EventArgs e) { }
+
+
+		private void tbAddress1_LostFocus(object sender, RoutedEventArgs e) { SetInfoText(); }
+
+		private void tbAddress2_LostFocus(object sender, RoutedEventArgs e) { SetInfoText(); }
+
+		private void tbCity_TextChanged(object sender, TextChangedEventArgs e) { SetInfoText(); }
+		private void tbFullName_LostFocus(object sender, RoutedEventArgs e) { SetInfoText(); }
+
+		private void tbZipCode_LostFocus(object sender, RoutedEventArgs e) { SetInfoText(); }
+		private void textBlock1_LostFocus(object sender, RoutedEventArgs e) { }
+
+		#endregion
+
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			base.OnNavigatedTo(e);
+
+			RefreshReminderList();
+		}
+
+		private void RefreshReminderList()
+		{
+			var reminders = ScheduledActionService.GetActions<Reminder>();
+			foreach (var r in reminders) if (r.BeginTime < DateTime.Now) ScheduledActionService.Remove(r.Name);
+			lbReminders.ItemsSource = ScheduledActionService.GetActions<Reminder>();
+		}
+
+		private void MakeReverseGeocodeRequest(Location l)
+		{
+			try {
+				// Set a Bing Maps key before making a request
+				const string KEY = "AkRhgqPR6aujo-xib-KiR8Lt20wsn89GY4R9SP0RA6h4w7QT9mS3kKwYKKxjklfV";
+
+				// Set the credentials using a valid Bing Maps key
+				// Set the point to use to find a matching address
+				var reverseGeocodeRequest = new ReverseGeocodeRequest {Credentials = new Credentials {ApplicationId = KEY}};
+
+				Location point = l;
+
+				reverseGeocodeRequest.Location = point;
+				reverseGeocodeRequest.UserProfile = new UserProfile {DeviceType = DeviceType.Mobile};
+
+				// Make the reverse geocode request
+				var geocodeService = new GeocodeServiceClient("BasicHttpBinding_IGeocodeService");
+				geocodeService.ReverseGeocodeCompleted += geocodeService_ReverseGeocodeCompleted;
+				geocodeService.ReverseGeocodeAsync(reverseGeocodeRequest);
+			} catch (Exception ex) {
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		public void MakeGeocodeRequest(Address a)
+		{
+			try {
+				// Set a Bing Maps key before making a request
+				const string KEY = "AkRhgqPR6aujo-xib-KiR8Lt20wsn89GY4R9SP0RA6h4w7QT9mS3kKwYKKxjklfV";
+
+				// Set the credentials using a valid Bing Maps key
+				// Set the point to use to find a matching address
+				var geocodeRequest = new GeocodeRequest {Credentials = new Credentials {ApplicationId = KEY}};
+
+				geocodeRequest.Address = a;
+				geocodeRequest.UserProfile = new UserProfile {DeviceType = DeviceType.Mobile};
+
+				// Make the reverse geocode request
+				var geocodeService = new GeocodeServiceClient("BasicHttpBinding_IGeocodeService");
+				geocodeService.GeocodeCompleted += geocodeService_GeocodeCompleted;
+				geocodeService.GeocodeAsync(geocodeRequest);
+			} catch (Exception ex) {
+				MessageBox.Show(ex.Message);
+			}
 		}
 
 		private void SetInfoText()
 		{
 			lblInfo_Address.Text = string.Format(_address, tbAddress1.Text, tbAddress2.Text);
 			lblInfo_FullName.Text = string.Format(_fullName, tbFullName.Text);
-			lblInfo_CityStateZip.Text = string.Format(_cityStateZip, tbCity.Text, lbiState.SelectedIndex >= 0 ? ((States)lbiState.Items[lbiState.SelectedIndex]).Text : string.Empty, tbZipCode.Text);
+			lblInfo_CityStateZip.Text = string.Format(_cityStateZip, tbCity.Text, tbDistrict.Text, tbZipCode.Text);
+			if (!string.IsNullOrEmpty(tbAddress1.Text) && !string.IsNullOrEmpty(tbCity.Text) && !string.IsNullOrEmpty(tbDistrict.Text))
+				MakeGeocodeRequest(new Address {AddressLine = tbAddress1.Text, AdminDistrict = tbDistrict.Text, Locality = tbCity.Text, PostalCode = tbZipCode.Text, CountryRegion = _currentBingGeocodeLocation.Address.CountryRegion});
 		}
 
-		private void tbFullName_LostFocus(object sender, RoutedEventArgs e)
+		private void tbNotes_GotFocus(object sender, RoutedEventArgs e)
 		{
-			SetInfoText();
+			if(tbNotes.Text.Equals("Notes...", StringComparison.InvariantCultureIgnoreCase))
+				tbNotes.Text = string.Empty;
 		}
 
-		private void tbAddress1_LostFocus(object sender, RoutedEventArgs e)
+		private void tbNotes_LostFocus(object sender, RoutedEventArgs e)
 		{
-			SetInfoText();
+			if (tbNotes.Text.Length <= 0) tbNotes.Text = "Notes...";
 		}
 
-		private void tbAddress2_LostFocus(object sender, RoutedEventArgs e)
+		private void bAddVisit_Click(object sender, RoutedEventArgs e)
 		{
-			SetInfoText();
-		}
-
-		private void tbCity_TextChanged(object sender, TextChangedEventArgs e)
-		{
-			SetInfoText();
-		}
-
-		private void lbiState_LostFocus(object sender, RoutedEventArgs e)
-		{
-			SetInfoText();
-		}
-
-		private void tbZipCode_LostFocus(object sender, RoutedEventArgs e)
-		{
-			SetInfoText();
-		}
-
-		private void Pivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
-			SetInfoText();
-		}
-
-		private void appbar_save_Click(object sender, EventArgs e)
-		{
-
-		}
-
-		private void miAddPicture_Click(object sender, EventArgs e)
-		{
-
-		}
-
-		private void appbar_cancel_Click(object sender, EventArgs e)
-		{
-			NavigationService.GoBack();
-		}
-
-		private void miGetDirection_Click(object sender, EventArgs e)
-		{
-
+			try {
+				
+			} finally {
+				MessageBox.Show("Visit Added.");
+			}
 		}
 	}
 }
---
+++ MyTime/MyTime/Model/TimeDataContext.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace MyTime.Model
+{
+	[Table]
+	public class TimeData : INotifyPropertyChanged, INotifyPropertyChanging
+	{
+
+		// Define ID: private field, public property, and database column.
+		private int _itemId;
+
+		[Column(IsPrimaryKey = true, IsDbGenerated = true, DbType = "INT NOT NULL Identity", CanBeNull = false, AutoSync = AutoSync.OnInsert)]
+		public int ItemId
+		{
+			get { return _itemId; }
+			set
+			{
+				if (_itemId != value) {
+					NotifyPropertyChanging("ItemId");
+					_itemId = value;
+					NotifyPropertyChanged("ItemId");
+				}
+			}
+		}
+
+		// Define item name: private field, public property, and database column.
+		private DateTime _date;
+
+		[Column]
+		public DateTime Date
+		{
+			get { return _date; }
+			set
+			{
+				if (_date != value) {
+					NotifyPropertyChanging("Date");
+					_date = value;
+					NotifyPropertyChanged("Date");
+				}
+			}
+		}
+
+		// Define completion value: private field, public property, and database column.
+		private int _minutes;
+
+		[Column]
+		public int Minutes
+		{
+			get { return _minutes; }
+			set
+			{
+				if (_minutes != value) {
+					NotifyPropertyChanging("Minutes");
+					_minutes = value;
+					NotifyPropertyChanged("Minutes");
+				}
+			}
+		}
+
+		// Version column aids update performance.
+		[Column(IsVersion = true)]
+		private Binary _version;
+
+		#region INotifyPropertyChanged Members
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		// Used to notify that a property changed
+		private void NotifyPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null) {
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+		#endregion
+
+		#region INotifyPropertyChanging Members
+
+		public event PropertyChangingEventHandler PropertyChanging;
+
+		// Used to notify that a property is about to change
+		private void NotifyPropertyChanging(string propertyName)
+		{
+			if (PropertyChanging != null) {
+				PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+			}
+		}
+
+		#endregion
+	}
+
+	
+}
