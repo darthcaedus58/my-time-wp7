6eeeac61acba461fa48b42b0097dfa10b3989689
tk@square-hiptobe.com <tk@square-hiptobe.com@863d6dba-c7e0-1ce1-ce57-a33eb1fcf9db>
2013-01-04 04:38:30 +0000

    Fixed Bug in send service report.
    Added numberpicker.

---
+++ trunk/MyTime/Controls/DigitPickerButton.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Controls
+{
+    public partial class DigitPickerButton : Button
+    {
+        //public string Text { get { return tbValue.Text; } set { tbValue.Text = value; } }
+
+        //public string Label { get { return tbLabel.Text; } set { tbLabel.Text = value; } }
+
+        //public DigitPickerButton()
+        //{
+        //    InitializeComponent();
+        //}
+
+        protected override void OnClick()
+        {
+            base.OnClick();
+        }
+    }
+}
