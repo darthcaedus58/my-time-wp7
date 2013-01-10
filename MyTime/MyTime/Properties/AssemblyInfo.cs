1a7d39ebdc2504b6f33da6a8d3637f03d8b42a36
tk <tk@square-hiptobe.com>
2013-01-10 15:49:46 +0000

    Updated S/W Versions for MainApp and DatabaseLib.

--- MyTime/MyTime/Properties/AssemblyInfo.cs
+++ MyTime/MyTime/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2013.1.5.1728")]
-[assembly: AssemblyFileVersion("1.13.1.5")]
+[assembly: AssemblyVersion("2013.1.10.1048")]
+[assembly: AssemblyFileVersion("1.13.1.10")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
