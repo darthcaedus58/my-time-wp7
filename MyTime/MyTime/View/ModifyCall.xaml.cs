4b0effbd72bde1469c5829604fbb4d997a5bceb7
tk <tk@square-hiptobe.com>
2013-01-30 05:48:04 +0000

    Updated ModifyCall.xaml to follow proper MVVM Pattern and Data Bindings.

--- MyTime/MyTime/View/ModifyCall.xaml.cs
+++ MyTime/MyTime/View/ModifyCall.xaml.cs
@@ -17,135 +17,80 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Navigation;
+using FieldService.ViewModels;
 using Microsoft.Phone.Controls;
 using MyTimeDatabaseLib;
 
 namespace FieldService.View
 {
-    /// <summary>
-    /// Class ModifyCall
-    /// </summary>
-    public partial class ModifyCall : PhoneApplicationPage
-    {
-        /// <summary>
-        /// The _call id
-        /// </summary>
-        private int _callId = -1;
+	/// <summary>
+	/// Class ModifyCall
+	/// </summary>
+	public partial class ModifyCall : PhoneApplicationPage
+	{
+		private PreviousVisitViewModel ViewModel { get { return ((PreviousVisitViewModel) DataContext); } }
 
-        /// <summary>
-        /// The _RV item id
-        /// </summary>
-        private int _rvItemId = -1;
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ModifyCall" /> class.
+		/// </summary>
+		public ModifyCall() { InitializeComponent(); }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ModifyCall" /> class.
-        /// </summary>
-        public ModifyCall() { InitializeComponent(); }
+		#region Events
 
-        #region Events
+		/// <summary>
+		/// Handles the Click event of the ApplicationBarIconButtonSave control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+		private void ApplicationBarIconButtonSave_Click(object sender, EventArgs e)
+		{
+			tbNotes.GetBindingExpression(PhoneTextBox.TextProperty).UpdateSource();
+			App.ToastMe(ViewModel.AddOrUpdateItem() ? "Call Saved." : "Call Saving Failed.");
+		}
 
-        /// <summary>
-        /// Handles the Click event of the ApplicationBarIconButtonSave control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
-        private void ApplicationBarIconButtonSave_Click(object sender, EventArgs e)
-        {
-            var call = new RvPreviousVisitData();
-            if (_callId >= 0) call = RvPreviousVisitsDataInterface.GetCall(_callId);
-            call.RvItemId = _rvItemId;
-            call.Books = (int) tbBooks.Value;
-            call.Brochures = (int) tbBrochures.Value;
-            call.Magazines = (int) tbMags.Value;
-            call.Notes = tbNotes.Text;
-            call.Date = (dpDatePicker.Value ?? DateTime.Now);
+		/// <summary>
+		/// Handles the Click event of the abmiDeleteCall control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+		private void abmiDeleteCall_Click(object sender, EventArgs e)
+		{
+			if (ViewModel.PreviousVisitItemId < 0 || MessageBox.Show("Are you sure you want to delete this call?", "FIELD SERVICE", MessageBoxButton.OKCancel) != MessageBoxResult.OK) return;
+			bool v = ViewModel.DeleteCall();
+			if (!v) {
+				App.ToastMe("Deleting the call failed.");
+				return;
+			}
+			NavigationService.GoBack();
+		}
 
+		#endregion
 
-            try {
-                _callId = RvPreviousVisitsDataInterface.SaveCall(call, true);
-                App.ToastMe(string.Format("Call {0}.", _callId >= 0 ? "Saved" : "Added"));
-            } catch (Exception ee) {
-                MessageBox.Show("Couldn't save call.\n\n\nException: " + ee.Message);
-            }
-        }
+		/// <summary>
+		/// Called when a page becomes the active page in a frame.
+		/// </summary>
+		/// <param name="e">An object that contains the event data.</param>
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			base.OnNavigatedTo(e);
+			if (!NavigationContext.QueryString.ContainsKey("rvid")) {
+				MessageBox.Show("You must save the RV before adding a call.");
+				NavigationService.GoBack();
+				return;
+			}
+			if (ViewModel.PreviousVisitItemId >= 0) return; //we have already loaded the call data and the user is just modifying it before saving it.
+			string rvItemId = NavigationContext.QueryString["rvid"];
+			if (string.IsNullOrEmpty(rvItemId)) {
+				MessageBox.Show("You must save the RV before adding a call.");
+				NavigationService.GoBack();
+				return;
+			}
+			ViewModel.ReturnVisitItemId = int.Parse(rvItemId);
 
-        /// <summary>
-        /// Handles the KeyDown event of the TextBoxMasking control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="KeyEventArgs" /> instance containing the event data.</param>
-        private void TextBoxMasking_KeyDown(object sender, KeyEventArgs e)
-        {
-            Key[] goodKeys = {
-                                 Key.D0, Key.D1, Key.D2, Key.D3, Key.D4,
-                                 Key.D5, Key.D6, Key.D7, Key.D8, Key.D9,
-                                 Key.NumPad0, Key.NumPad1, Key.NumPad2, Key.NumPad3, Key.NumPad4,
-                                 Key.NumPad5, Key.NumPad6, Key.NumPad7, Key.NumPad8, Key.NumPad9
-                             };
-            if (!goodKeys.Contains(e.Key)) {
-                e.Handled = true;
-            }
-        }
+			if (!NavigationContext.QueryString.ContainsKey("id")) return;
 
-        /// <summary>
-        /// Handles the Click event of the abmiDeleteCall control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
-        private void abmiDeleteCall_Click(object sender, EventArgs e)
-        {
-            if (_callId < 0 || MessageBox.Show("Are you sure you want to delete this call?", "FIELD SERVICE", MessageBoxButton.OKCancel) != MessageBoxResult.OK) return;
-            RvPreviousVisitsDataInterface.DeleteCall(_callId);
-            App.ToastMe("Call Deleted.");
-            NavigationService.GoBack();
-        }
-
-        #endregion
-
-        /// <summary>
-        /// Called when a page becomes the active page in a frame.
-        /// </summary>
-        /// <param name="e">An object that contains the event data.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            base.OnNavigatedTo(e);
-            if (!NavigationContext.QueryString.ContainsKey("rvid")) {
-                NavigationService.GoBack();
-                return;
-            }
-            if (_callId >= 0) return; //we have already loaded the call data and the user is just modifying it before saving it.
-            string rvItemId = NavigationContext.QueryString["rvid"];
-            string callId = NavigationContext.QueryString.ContainsKey("id") ? NavigationContext.QueryString["id"] : string.Empty;
-            if (string.IsNullOrEmpty(rvItemId)) {
-                NavigationService.GoBack();
-                return;
-            }
-            try {
-                _rvItemId = int.Parse(rvItemId);
-                _callId = string.IsNullOrEmpty(callId) ? -1 : int.Parse(callId);
-
-                if (_callId >= 0) RetrieveCallInfo(_callId);
-            } catch (Exception) {}
-        }
-
-        /// <summary>
-        /// Retrieves the call info.
-        /// </summary>
-        /// <param name="callId">The call id.</param>
-        private void RetrieveCallInfo(int callId)
-        {
-            try {
-                RvPreviousVisitData call = RvPreviousVisitsDataInterface.GetCall(callId);
-
-                tbBooks.Value = call.Books;
-                tbBrochures.Value = call.Brochures;
-                tbMags.Value = call.Magazines;
-                tbNotes.Text = call.Notes;
-                dpDatePicker.Value = call.Date;
-                _callId = call.ItemId;
-            } catch (RvPreviousVisitNotFoundException e) {
-                MessageBox.Show(string.Format("{0}\n\nPlease create a new call.", e.Message));
-            }
-        }
-    }
+			string callId = NavigationContext.QueryString["id"];
+			ViewModel.PreviousVisitItemId = int.Parse(callId);
+		}
+	}
 }
