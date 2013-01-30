4df6e64489a8cf795e620e3cdc92c6bf2fe21d6c
tk@square-hiptobe.com <tk@square-hiptobe.com@863d6dba-c7e0-1ce1-ce57-a33eb1fcf9db>
2013-01-30 22:02:36 +0000

    Added HEaderControl.xaml UserControl
    Updated RegularTime.xaml to use it.

---
+++ trunk/MyTime/MyTime/HeaderControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace FieldService
+{
+	public partial class HeaderControl : UserControl
+	{
+		public string IconSource { get; set; }
+		public string HeaderText { get; set; }
+
+		public HeaderControl()
+		{
+			DataContext = this;
+			InitializeComponent();
+		}
+	}
+}
