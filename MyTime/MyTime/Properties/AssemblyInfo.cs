c9ff1b6d512428ddb04d8693dec239d7601b6e14
tk <tk@square-hiptobe.com>
2013-04-30 02:46:16 +0000

    Fixed Issue # 14.

--- MyTime/MyTime/Properties/AssemblyInfo.cs
+++ MyTime/MyTime/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2013.4.14.2009")]
-[assembly: AssemblyFileVersion("1.13.4.14")]
+[assembly: AssemblyVersion("2013.4.29.2243")]
+[assembly: AssemblyFileVersion("1.13.4.29")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
