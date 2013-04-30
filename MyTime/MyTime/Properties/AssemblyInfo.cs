6893fb5d484f99086f8d4a4ffe36e7879fec9244
tk <tk@square-hiptobe.com>
2013-04-30 02:47:08 +0000

    Fixed Issue #14

--- MyTime/MyTime/Properties/AssemblyInfo.cs
+++ MyTime/MyTime/Properties/AssemblyInfo.cs
@@ -15,6 +15,7 @@ using System.Resources;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+
 // SettingsCollection ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
