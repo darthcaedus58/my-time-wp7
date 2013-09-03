6cb42e15ad8e6e8e5a4969708ed27b8c6ff8cd7a
tk@square-hiptobe.com <tk@square-hiptobe.com@863d6dba-c7e0-1ce1-ce57-a33eb1fcf9db>
2013-09-03 22:16:01 +0000

    Added custom time report page.

---
+++ trunk/MyTime/MyTime/View/CustomReportsPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace FieldService.View
+{
+	public partial class CustomReportsPage : PhoneApplicationPage
+	{
+		public CustomReportsPage()
+		{
+			InitializeComponent();
+		}
+
+		private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e) 
+		{ 
+			//throw new NotImplementedException(); 
+			dpStartDate.Value = DateTime.Today;
+		}
+		
+		private void abibRunReport_Click(object sender, EventArgs e) 
+		{ 
+			//throw new NotImplementedException(); 
+			if (dpToDate.Value == null || dpStartDate.Value == null) {
+				App.ToastMe(StringResources.CustomReportsPage_ErrorText);
+				return;
+			}
+			DateTime from = dpStartDate.Value.Value;
+			DateTime to = dpToDate.Value.Value;
+
+			NavigationService.Navigate(new Uri(string.Format("/View/TimeReport.xaml?from={0}&to={1}", from.ToString("MM-dd-yyyy"), to.ToString("MM-dd-yyyy")), UriKind.Relative));
+		}
+
+		private void ButtonSetRangeWeek_Click(object sender, RoutedEventArgs e)
+		{
+			//throw new NotImplementedException();
+
+			if (dpStartDate.Value != null) dpToDate.Value = dpStartDate.Value.Value.AddDays(7);
+		}
+
+		private void ButtonSetRangeMonth_Click(object sender, RoutedEventArgs e)
+		{
+			//throw new NotImplementedException();
+			if (dpStartDate.Value != null) dpToDate.Value = dpStartDate.Value.Value.AddMonths(1).AddDays(-1);
+		}
+		private void ButtonSetRangeYear_Click(object sender, RoutedEventArgs e)
+		{
+			//throw new NotImplementedException();
+			if (dpStartDate.Value != null) dpToDate.Value = dpStartDate.Value.Value.AddYears(1).AddDays(-1);
+		}
+	}
+}
