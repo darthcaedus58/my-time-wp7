45460ac8e72e2faad3ce4efa6dda2474ac53053b
tk <tk@square-hiptobe.com>
2014-03-17 22:01:07 +0000

    Added custom increments for time.

---
+++ MyTime/MyTime/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FieldService.ViewModels;
+
+namespace FieldService
+{
+    public class AppSettingsProvider
+    {
+        private static SettingsViewModel _appSettings = new SettingsViewModel();
+
+        public SettingsViewModel AppSettings { get { return _appSettings; } }
+    }
+}
