8c5fa870b7c72c5dba19ebca9e814429a889f053
tk@square-hiptobe.com <tk@square-hiptobe.com@863d6dba-c7e0-1ce1-ce57-a33eb1fcf9db>
2014-07-01 20:29:41 +0000

    Added Time Calculator Popup.

---
+++ trunk/MyTime/MyTime/View/TimeCalcControl.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Telerik.Windows.Controls;
+
+namespace FieldService.View
+{
+    public class TimeCalcFormClosedEventArgs : EventArgs
+    {
+        public DialogResult DialogResult { get; set; }
+        public TimeSpan TimeSpan { get; set; }
+        public TimeCalcFormClosedEventArgs(DialogResult r, TimeSpan t)
+        {
+            DialogResult = r;
+            TimeSpan = t;
+        }
+    }
+    public partial class TimeCalcControl : UserControl
+    {
+        public event FormClosedEventHandler FormClosed;
+        public delegate void FormClosedEventHandler(object sender, TimeCalcFormClosedEventArgs e);
+ 
+        public TimeCalcControl()
+        {
+            InitializeComponent();
+            tpEnd.Value = DateTime.Now;
+
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            var start = tpStart.Value ?? DateTime.MinValue;
+            var end = tpEnd.Value ?? DateTime.MinValue;
+
+            TimeSpan t = end - start;
+
+            if (start == DateTime.MinValue || end == DateTime.MinValue)
+                FormClosed(this, new TimeCalcFormClosedEventArgs(DialogResult.Cancel, TimeSpan.Zero));
+
+            t = RoundTime(t);
+
+
+            FormClosed(this, new TimeCalcFormClosedEventArgs(DialogResult.OK, t));
+        }
+
+        private static TimeSpan RoundTime(TimeSpan t)
+        {
+            var ts =
+                TimeSpan.FromMinutes(App.Settings.roundTimeIncrement*
+                                     Math.Ceiling(t.TotalMinutes/App.Settings.roundTimeIncrement));
+            float m = float.Parse(t.TotalMinutes.ToString())%App.Settings.roundTimeIncrement;
+            if (m <= (App.Settings.roundTimeIncrement/2.0))
+                t = TimeSpan.FromMinutes(ts.TotalMinutes - App.Settings.roundTimeIncrement);
+            else t = ts;
+            return t;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            FormClosed(this, new TimeCalcFormClosedEventArgs(DialogResult.Cancel, TimeSpan.Zero));
+        }
+    }
+
+    
+}
