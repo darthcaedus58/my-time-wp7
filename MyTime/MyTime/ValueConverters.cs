1339a027e60fb9874c7ef8d6bafc48663f25f158
tk <tk@square-hiptobe.com>
2014-07-02 17:59:37 +0000

    related to calc on add time pages.

--- MyTime/MyTime/ValueConverters.cs
+++ MyTime/MyTime/ValueConverters.cs
@@ -9,7 +9,8 @@ using System.Windows.Data;
 
 namespace FieldService
 {
-        public class StringLengthToVisibilityConverter : IValueConverter
+        
+    public class StringLengthToVisibilityConverter : IValueConverter
         {
                 public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
                 {
@@ -21,7 +22,8 @@ namespace FieldService
                         return null;
                 }
         }
-        public class DateTimeToShortTimeValueConverter : IValueConverter
+        
+    public class DateTimeToShortTimeValueConverter : IValueConverter
         {
                 public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
                 {
@@ -34,7 +36,8 @@ namespace FieldService
                                 value.ToString(), Thread.CurrentThread.CurrentCulture.DateTimeFormat.ShortTimePattern, CultureInfo.CurrentCulture);
                 }
         }
-	public class DoubleToVisibilityValueConverter : IValueConverter
+	
+    public class DoubleToVisibilityValueConverter : IValueConverter
 	{
 		/// <summary>
 		/// Modifies the source data before passing it to the target for display in the UI.
@@ -54,7 +57,35 @@ namespace FieldService
 		/// <param name="value">The target data being passed to the source.</param><param name="targetType">The <see cref="T:System.Type"/> of data expected by the source object.</param><param name="parameter">An optional parameter to be used in the converter logic.</param><param name="culture">The culture of the conversion.</param>
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) { return (Visibility) value == Visibility.Collapsed ? (double)0.0 : (double)1.0; }
 	}
-	public class MinutesToTimeSpanValueConverter : IValueConverter
+
+    public class MinutesToTimeSpanValueConverter : IValueConverter
+    {
+        /// <summary>
+        /// Modifies the source data before passing it to the target for display in the UI.
+        /// </summary>
+        /// <returns>
+        /// The value to be passed to the target dependency property.
+        /// </returns>
+        /// <param name="value">The source data being passed to the target.</param><param name="targetType">The <see cref="T:System.Type"/> of data expected by the target dependency property.</param><param name="parameter">An optional parameter to be used in the converter logic.</param><param name="culture">The culture of the conversion.</param>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new TimeSpan(0, (int) value, 0);
+        }
+
+        /// <summary>
+        /// Modifies the target data before passing it to the source object.  This method is called only in <see cref="F:System.Windows.Data.BindingMode.TwoWay"/> bindings.
+        /// </summary>
+        /// <returns>
+        /// The value to be passed to the source object.
+        /// </returns>
+        /// <param name="value">The target data being passed to the source.</param><param name="targetType">The <see cref="T:System.Type"/> of data expected by the source object.</param><param name="parameter">An optional parameter to be used in the converter logic.</param><param name="culture">The culture of the conversion.</param>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (int) ((TimeSpan) value).TotalMinutes;
+        }
+    }
+
+    public class MinutesToDateTimeValueConverter : IValueConverter
 	{
 		/// <summary>
 		/// Modifies the source data before passing it to the target for display in the UI.
@@ -65,7 +96,7 @@ namespace FieldService
 		/// <param name="value">The source data being passed to the target.</param><param name="targetType">The <see cref="T:System.Type"/> of data expected by the target dependency property.</param><param name="parameter">An optional parameter to be used in the converter logic.</param><param name="culture">The culture of the conversion.</param>
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			return new TimeSpan(0, (int)value, 0);
+            return new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, (int)value, 0);
 		}
 
 		/// <summary>
@@ -77,10 +108,11 @@ namespace FieldService
 		/// <param name="value">The target data being passed to the source.</param><param name="targetType">The <see cref="T:System.Type"/> of data expected by the source object.</param><param name="parameter">An optional parameter to be used in the converter logic.</param><param name="culture">The culture of the conversion.</param>
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			return (int)((TimeSpan)value).TotalMinutes;
+		    return (int) ((DateTime) value).TimeOfDay.Minutes;
 		}
 	}
-        public class StringToUpperConverter : IValueConverter
+        
+    public class StringToUpperConverter : IValueConverter
         {
                 public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
                 {
@@ -92,6 +124,4 @@ namespace FieldService
                         throw new NotImplementedException();
                 }
         }
-
-
 }
