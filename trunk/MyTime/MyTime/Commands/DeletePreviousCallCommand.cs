342346e9483149017436f8bb4128870936783edd
tk@square-hiptobe.com <tk@square-hiptobe.com@863d6dba-c7e0-1ce1-ce57-a33eb1fcf9db>
2014-07-15 19:35:22 +0000

    Added Context menu to delete previous visits.

---
+++ trunk/MyTime/MyTime/Commands/DeletePreviousCallCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using MyTimeDatabaseLib;
+
+namespace FieldService.Commands
+{
+    public class DeletePreviousCallCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return parameter is int;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (!CanExecute(parameter)) return;
+
+            var itemId = (int) parameter;
+
+            if (itemId < 0 || MessageBox.Show(StringResources.AddCallPage_DeleteCallConfirmation, StringResources.ApplicationName, MessageBoxButton.OKCancel) != MessageBoxResult.OK) return;
+            bool v = RvPreviousVisitsDataInterface.DeleteCall(itemId);
+            if (!v) {
+                App.ToastMe(StringResources.AddCallPage_DeleteCallFailed);
+            }
+            App.ViewModel.ReturnVisitData.LoadPreviousVisits(App.ViewModel.ReturnVisitData.ItemId);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
