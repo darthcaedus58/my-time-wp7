bfc5c00308846d88858dc07c356d2c5e9860c5c4
tk <tk@square-hiptobe.com>
2014-07-15 18:40:21 +0000

    Add context menu to main page rv list.

---
+++ MyTime/MyTime/Commands/AddVisitToRVCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+
+namespace FieldService.Commands
+{
+    public class AddVisitToRVCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return parameter is int;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (!CanExecute(parameter)) return;
+            var itemId = (int)parameter;
+
+            (Application.Current.RootVisual as PhoneApplicationFrame).Navigate(new Uri(string.Format("/View/PreviousCall.xaml?rvid={0}", itemId), UriKind.Relative));
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ MyTime/MyTime/Commands/DeleteReturnVisitCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using MyTimeDatabaseLib;
+using Telerik.Windows.Controls;
+
+namespace FieldService.Commands
+{
+    public class DeleteReturnVisitCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return parameter is int;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (!CanExecute(parameter)) return;
+            var itemId = (int) parameter;
+
+            if (MessageBox.Show(StringResources.RVPage_Messages_Delete, 
+                StringResources.ApplicationName,
+                    MessageBoxButton.OKCancel) == MessageBoxResult.Cancel) return;
+
+            try {
+                App.ViewModel.IsRvDataChanged = true;
+                var deleteCalls = App.Settings.deleteCallsAndRv;
+                App.ToastMe(ReturnVisitsInterface.DeleteReturnVisit(itemId, deleteCalls)
+                    ? "Deleted."
+                    : StringResources.AddTimePage_Messages_TimeDeleteFailed);
+            }
+            catch {
+                App.ToastMe(ReturnVisitsInterface.DeleteReturnVisit(itemId,
+                    (MessageBox.Show(StringResources.RVPage_Messages_IncludeAllVisits) == MessageBoxResult.OK))
+                    ? "Deleted"
+                    : StringResources.AddTimePage_Messages_TimeDeleteFailed);
+            }
+            finally {
+                App.ViewModel.LoadReturnVisitList(SortOrder.DateNewestToOldest);
+            }
+
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
