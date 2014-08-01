87467eb56548377f632ab3024aee1369dd13dfe9
tk@square-hiptobe.com <tk@square-hiptobe.com@863d6dba-c7e0-1ce1-ce57-a33eb1fcf9db>
2014-08-01 19:28:54 +0000

    Properties changes to capabilities.

--- trunk/MyTime/MyTime/Properties/AssemblyInfo.cs
+++ trunk/MyTime/MyTime/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2014.7.18.1511")]
-[assembly: AssemblyFileVersion("1.14.7.18")]
+[assembly: AssemblyVersion("2014.8.1.1519")]
+[assembly: AssemblyFileVersion("1.14.8.1")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
